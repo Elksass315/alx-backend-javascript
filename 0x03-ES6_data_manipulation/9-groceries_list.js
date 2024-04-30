bfcc4c85e19b4e1e2dc61cee1d7eb86bb730b105
Elksass <qsasomar@gmail.com>
2024-05-01 01:34:32 +0300

    Fix eslint

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,9 +1,9 @@
 export default function groceriesList() {
-    return new Map([
-        ["apples", 10],
-        ["Tomatoes", 10],
-        ["Pasta", 1],
-        ["Rice", 1],
-        ["Banana", 5]
-    ]);
+  return new Map([
+    ['apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
 }
