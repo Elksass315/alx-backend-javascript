cd8d8b18ed28ff503b4178d57c3d3f09f34bc824
Elksass <qsasomar@gmail.com>
2024-05-01 00:29:50 +0300

    Typed Arrays

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,3 +1,7 @@
 export default function createInt8TypedArray(length, postion, value) {
-    
+  if (postion < 0 || postion >= length) throw new Error('Position outside range');
+  const buffer = new ArrayBuffer(length);
+  const array = new Int8Array(buffer);
+  array[postion] = value;
+  return new DataView(buffer);
 }
